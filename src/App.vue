df52c3a9b7cc4e58d4be9de6da0af1147df17206
DELL28\Patrick Harsch <patrickh@ensemble.com>
2022-04-28 11:54:52 -0700

    completed authentication setup

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,52 @@
 <template>
   <div id="app">
-    <h1>Todo App</h1>
-    <input type="text" v-model="name" placeholder="Todo name">
-    <input type="text" v-model="description" placeholder="Todo description">
-    <button v-on:click="createTodo">Create Todo</button>
-    <div v-for="item in todos" :key="item.id">
-      <h3>{{ item.name }}</h3>
-      <p>{{ item.description }}</p>
-    </div>
+    <div v-if="authState !== 'signedin'">You are signed out.</div>
+    <amplify-authenticator>
+      <div v-if="authState === 'signedin' && user">
+        <h1>Todo App</h1>
+        <input type="text" v-model="name" placeholder="Todo name" />
+        <input
+          type="text"
+          v-model="description"
+          placeholder="Todo description"
+        />
+        <button v-on:click="createTodo">Create Todo</button>
+        <div v-for="item in todos" :key="item.id">
+          <h3>{{ item.name }}</h3>
+          <p>{{ item.description }}</p>
+        </div>
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
-import { API } from 'aws-amplify';
-import { createTodo } from './graphql/mutations';
-import { listTodos } from './graphql/queries';
-import { onCreateTodo } from './graphql/subscriptions';
+import { API } from "aws-amplify";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import { onCreateTodo } from "./graphql/subscriptions";
+import { onAuthUIStateChange } from "@aws-amplify/ui-components";
 
 export default {
-  name: 'App',
+  name: "App",
   async created() {
     this.getTodos();
     this.subscribe();
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    });
   },
   data() {
     return {
-      name: '',
-      description: '',
-      todos: []
-    }
+      name: "",
+      description: "",
+      todos: [],
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined,
+    };
   },
   methods: {
     async createTodo() {
@@ -38,27 +56,29 @@ export default {
       this.todos = [...this.todos, todo];
       await API.graphql({
         query: createTodo,
-        variables: {input: todo},
+        variables: { input: todo },
       });
-      this.name = '';
-      this.description = '';
+      this.name = "";
+      this.description = "";
     },
     async getTodos() {
       const todos = await API.graphql({
-        query: listTodos
+        query: listTodos,
       });
       this.todos = todos.data.listTodos.items;
     },
     subscribe() {
-      API.graphql({ query: onCreateTodo })
-        .subscribe({
-          next: (eventData) => {
-            let todo = eventData.value.data.onCreateTodo;
-            if (this.todos.some(item => item.name === todo.name)) return; // remove duplications
-            this.todos = [...this.todos, todo];
-          }
-        });
-    }
-  }
-}
+      API.graphql({ query: onCreateTodo }).subscribe({
+        next: (eventData) => {
+          let todo = eventData.value.data.onCreateTodo;
+          if (this.todos.some((item) => item.name === todo.name)) return; // remove duplications
+          this.todos = [...this.todos, todo];
+        },
+      });
+    },
+  },
+  beforeDestroy() {
+    this.unsubscribeAuth();
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-Vue.config.productionTip = false
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
+Vue.config.productionTip = false;
+Vue.config.ignoredElements = [/amplify-\w*/];
 
 new Vue({
   router,
